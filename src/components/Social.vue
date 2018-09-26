2e448e5e3d3ee321dab2add85d21b5a8995cfe87
Mikhail Delport <mikhail@delport.ca>
2018-09-26 13:02:10 -0400

    I swear I can program

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -3,14 +3,14 @@
     <div v-for="site in sites" :key="site.url">
       <div v-if="site.name.toLowerCase() == 'twitter'">
         <img height="32" width="32" src="https://unpkg.com/simple-icons@latest/icons/twitter.svg" />
-        <a :href="site.url">
+        <a :href="site.url">{{ site.name }}</a>
       </div>
       <div v-else-if="site.name.toLowerCase() == 'facebook'">
         <img height="32" width="32" src="https://unpkg.com/simple-icons@latest/icons/facebook.svg" />
-        <a :href="site.url">
+        <a :href="site.url">{{ site.name }}</a>
       </div>
       <div v-else>
-        <a :href="site.url">
+        <a :href="site.url">{{ site.name }}</a>
       </div>
 
     </div>
