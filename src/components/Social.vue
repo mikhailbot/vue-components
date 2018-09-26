7a71799992b3557d363af9503f4962c5cbfe01fa
Mikhail Delport <mikhail@delport.ca>
2018-09-26 12:41:11 -0400

    Simple Social component

---
+++ src/components/Social.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div v-for="site in sites" :key="site.url">
+      {{ site.name }}: {{ site.url}}
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  props: ["sites"]
+};
+</script>
+
+<style>
+</style>
