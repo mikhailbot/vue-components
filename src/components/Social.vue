715a1500d361aad18b957347d18251852249719d
Mikhail Delport <mikhail@delport.ca>
2018-09-26 12:55:22 -0400

    Mikhail is bad at programming

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div v-for="site in sites" :key="site.url">
-      <span v-if="site.name.toLowercase() == 'twitter'">
+      <span v-if="site.name.toLowerCase() == 'twitter'">
         <img height="32" width="32" src="https://unpkg.com/simple-icons@latest/icons/twitter.svg" />
         <a href={{ site.url}}>
       </span>
-      <span v-else-if="site.name.toLowercase() == 'facebook'">
+      <span v-else-if="site.name.toLowerCase() == 'facebook'">
         <img height="32" width="32" src="https://unpkg.com/simple-icons@latest/icons/facebook.svg" />
         <a href={{ site.url}}>
       </span>
