d99cd75759dd22a3ea0e07963e6de206e9f3fdb6
Mikhail Delport <mikhail@delport.ca>
2018-09-26 12:57:25 -0400

    Switch to divs

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
     <div v-for="site in sites" :key="site.url">
-      <span v-if="site.name.toLowerCase() == 'twitter'">
+      <div v-if="site.name.toLowerCase() == 'twitter'">
         <img height="32" width="32" src="https://unpkg.com/simple-icons@latest/icons/twitter.svg" />
         <a href={{ site.url}}>
-      </span>
-      <span v-else-if="site.name.toLowerCase() == 'facebook'">
+      </div>
+      <div v-else-if="site.name.toLowerCase() == 'facebook'">
         <img height="32" width="32" src="https://unpkg.com/simple-icons@latest/icons/facebook.svg" />
         <a href={{ site.url}}>
-      </span>
-      <span v-else>
+      </div>
+      <div v-else>
         <a href={{ site.url}}>
-      </span>
+      </div>
 
     </div>
   </div>
